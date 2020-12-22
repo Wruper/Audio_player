9b10f8ac6defc81b2ea6830870fe5413fe49f69b
Wruper <ricards1033@gmail.com>
2020-12-22 18:21:03 +0200

    Testing how to push into github

--- Audio_player/MainWindow.xaml.cs
+++ Audio_player/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace Audio_player
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// This is a test to see how to properly push into github using VS
     public partial class MainWindow : Window
     {
         public MainWindow()
