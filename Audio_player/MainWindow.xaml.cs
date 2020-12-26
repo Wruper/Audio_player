79cd5956affaf54d21722887b92135d3f59ff09b
Wruper <ricards1033@gmail.com>
2020-12-26 14:37:03 +0200

    Added the functionality of folder, play and stop button. Aswell changed the title and the icon of the application.

--- Audio_player/MainWindow.xaml.cs
+++ Audio_player/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Windows;
-
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Audio_player
 {
@@ -11,9 +13,34 @@ namespace Audio_player
     /// This is a test to see how to properly push into github using VS 
     public partial class MainWindow : Window
     {
+
+        private MediaPlayer audioPlayer = new MediaPlayer();
+
+        private bool audioPlayerIsPlaying = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bttnFolder_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Media files (*.mp3;*.mpg;*.mpeg;*.mp4)|*.mp3;*.mpg;*.mpeg;*.mp4|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+                audioPlayer.Open(new Uri(openFileDialog.FileName));
+        }
+
+        private void bttnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            audioPlayer.Play();
+            audioPlayerIsPlaying = true;
+        }
+
+
+        private void bttnPause_Click(object sender, RoutedEventArgs e)
+        {
+            audioPlayer.Pause();
         }
     }
 }
