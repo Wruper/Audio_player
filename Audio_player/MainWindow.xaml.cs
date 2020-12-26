c003f042646c7924784c0a9133a414a87d78531c
Wruper <ricards1033@gmail.com>
2020-12-26 17:18:24 +0200

    Made it so the seeker moves and the audio files length and current time is shown. Aswell made it so that if someone preses on current time, it shows how many seconds are left in the track.

--- Audio_player/MainWindow.xaml.cs
+++ Audio_player/MainWindow.xaml.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace Audio_player
 {
@@ -15,11 +17,23 @@ namespace Audio_player
     {
 
         private MediaPlayer audioPlayer = new MediaPlayer();
+        String currentSong;
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
+        private bool reverseTime = false;
+        private bool isOpened = false;
+        private bool isPausedByClick = false;
+
 
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
+        /* Buttons */
 
         private void bttnFolder_Click(object sender, RoutedEventArgs e)
         {
@@ -27,7 +41,9 @@ namespace Audio_player
             openFileDialog.Filter = "Media files (*.mp3;*.mpg;*.mpeg;*.mp4)|*.mp3;*.mpg;*.mpeg;*.mp4|All files (*.*)|*.*";
             if (openFileDialog.ShowDialog() == true)
                 audioPlayer.Open(new Uri(openFileDialog.FileName));
-            songName.Text = openFileDialog.SafeFileName;
+                isOpened = true;
+                currentSong = openFileDialog.SafeFileName;
+                songName.Text = currentSong.Substring(0,currentSong.Length - 4); // Removes the '.mp3' from song name.
                 audioPlayer.Play();
         }   
 
@@ -41,5 +57,63 @@ namespace Audio_player
         {
             audioPlayer.Pause();
         }
+
+        /* Navigation bar */
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if ((isOpened == true) && (audioPlayer.NaturalDuration.HasTimeSpan) && (!userIsDraggingSlider))
+            {
+                sliderProgress.Minimum = 0;
+                sliderProgress.Maximum = audioPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+                sliderProgress.Value = audioPlayer.Position.TotalSeconds;
+                setLength();
+                
+            }
+        }
+
+        private void setLength()
+        {
+            lenght.Text = TimeSpan.FromSeconds(sliderProgress.Maximum).ToString(@"hh\:mm\:ss");
+        }
+
+        private void timeUpdate()
+        {
+            if (!reverseTime)
+            {
+                currentTime.Text = TimeSpan.FromSeconds(sliderProgress.Value).ToString(@"hh\:mm\:ss");
+            }
+            else
+            {
+                currentTime.Text = "-" + TimeSpan.FromSeconds(sliderProgress.Maximum - sliderProgress.Value).ToString(@"hh\:mm\:ss");
+            }
+        }
+
+
+        private void sliderProgress_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            timeUpdate();
+        }
+
+
+        private void currentTime_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            reverseTime = !reverseTime;
+            timeUpdate();
+
+        }
+
+        private void sliderProgress_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            userIsDraggingSlider = false;
+            audioPlayer.Position = TimeSpan.FromSeconds(sliderProgress.Value);
+        }
+
+        private void sliderProgress_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            userIsDraggingSlider = true;
+        }
+
+
+
     }
 }
